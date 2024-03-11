5ea52b8aca60b3d115e92f0cbbef929f73d5b427
MunawarShaik <munawarshaik8328@gmail.com>
2024-03-11 16:58:05 +0530

    working on login page

--- src/app/_components/login.js
+++ src/app/_components/login.js
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleLogin = () => {
+    console.log(email, password);
+
+    if (!email || !password) {
+      setError(true);
+      return false;
+    } else {
+      setError(false);
+    }
+  };
   return (
     <>
       <h1>Login</h1>
@@ -8,17 +24,29 @@ const Login = () => {
             type="text"
             placeholder="Enter email id"
             className="inputField"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
+          {error && !email && (
+            <span className="inputError">Enter valid email</span>
+          )}
         </div>
         <div className="inputWrapper">
           <input
             type="password"
             className="inputField"
             placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
+          {error && !password && (
+            <span className="inputError">Enter valid password</span>
+          )}
         </div>
         <div className="inputWrapper">
-          <button className="button">Login</button>
+          <button className="button" onClick={handleLogin}>
+            Login
+          </button>
         </div>
       </div>
     </>
